ec07a13b7b74843ce7a08f5a758a3b92e19faeb0
Gumbraise <34947108+Gumbraise@users.noreply.github.com>
2026-04-25 11:34:24 +0200

    feat(credentials): add Strava OAuth2 credential

---
+++ credentials/StravaOAuth2Api.credentials.ts
@@ -0,0 +1,43 @@
+import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class StravaOAuth2Api implements ICredentialType {
+	name = 'stravaOAuth2Api';
+	displayName = 'Strava OAuth2 API';
+	extends = ['oAuth2Api'];
+	icon = { light: 'file:../nodes/Strava/strava.svg', dark: 'file:../nodes/Strava/strava.svg' } as const;
+	documentationUrl = 'https://developers.strava.com/docs/authentication/';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Grant Type',
+			name: 'grantType',
+			type: 'hidden',
+			default: 'authorizationCode',
+		},
+		{
+			displayName: 'Authorization URL',
+			name: 'authUrl',
+			type: 'hidden',
+			default: 'https://www.strava.com/oauth/authorize',
+		},
+		{
+			displayName: 'Access Token URL',
+			name: 'accessTokenUrl',
+			type: 'hidden',
+			default: 'https://www.strava.com/oauth/token',
+		},
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden',
+			default:
+				'read,read_all,profile:read_all,profile:write,activity:read,activity:read_all,activity:write',
+		},
+		{
+			displayName: 'Authentication',
+			name: 'authentication',
+			type: 'hidden',
+			default: 'header',
+		},
+	];
+}
